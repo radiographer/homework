df09022375aac1e91a8bec8dc3e1547e9fc3b0e3
radiographer <lukas.w@gmx.com>
2021-04-08 15:35:30 +0200

    9/10

--- lesson9/Javascript/jsTasks.js
+++ lesson9/Javascript/jsTasks.js
@@ -56,8 +56,79 @@ console.log(number + number + stringNumber);
 
 // 6
 let number = 1;
-let number = 2;
-let number = 3;
+number = 2;
+number = 3;
 console.log(number);
 
 // 7
+function add() {
+  console.log(2 + 2);
+}
+
+function string() {
+  console.log("Hello World!");
+}
+
+function bolean() {
+  console.log(true);
+}
+
+function modulo(a, b) {
+  console.log(a % b);
+}
+modulo(6, 4);
+
+function multiple(a, b) {
+  console.log(a * b);
+}
+multiple(5, 60);
+multiple(3, 3600);
+
+function calc(a, b, c) {
+  console.log((a * b) / c);
+}
+calc(10, 9, 2);
+
+function minus(a, b) {
+  console.log(a - b);
+}
+minus(10, 200);
+
+// 8
+let arr = [1, 2, 3];
+
+let arr = ["Skoda", "Audi", "BMW"];
+
+let arr = [true, false, true];
+
+let arr = [];
+
+let arr = [true, "Maciek", 3.14, false, "Warning"];
+console.log(arr[1]);
+
+let arr = [1, 2, 3];
+console.log(arr.length);
+
+let arr = [];
+arr.push(2);
+console.log(arr);
+
+// 9
+let obj = {};
+
+let person = {
+  name: "Iza",
+};
+
+let personDetails = {
+  name: "Łukasz",
+  lastName: "Walczak",
+  age: 39,
+  drivingLicense: true,
+};
+
+console.log(personDetails.age);
+
+console.log(personDetails);
+
+// 10
