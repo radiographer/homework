912e6e95e8e2c85977a4ece8b277e768830d4e7a
radiographer <lukas.w@gmx.com>
2021-04-08 11:20:21 +0200

    6/10

---
+++ lesson9/Javascript/jsTasks.js
@@ -0,0 +1,63 @@
+// 1
+let name = "Karol";
+
+let age = 40;
+
+let isOpen = true;
+
+let stringBoolean = "true";
+
+let stringNumber = "100";
+
+// 2
+let name = "Michał";
+console.log(name);
+
+let hello = "Hello Ania";
+console.log(hello);
+
+let number = 4;
+console.log(number);
+
+let boolean = false;
+console.log(boolean);
+
+// 3
+let name = "Karol";
+console.log(name);
+
+let age = 40;
+console.log(age);
+
+let isOpen = true;
+console.log(isOpen);
+
+let stringBoolean = "true";
+console.log(stringBoolean);
+
+let stringNumber = "100";
+console.log(stringNumber);
+
+// 4
+let number = 2;
+console.log(number + number);
+
+let name = "Michał ";
+let surname = "Kowalski";
+console.log(name + surname);
+
+let number = 2;
+console.log(number + number * number);
+
+// 5
+let stringNumber = ", 4";
+let number = 4;
+console.log(number + number + stringNumber);
+
+// 6
+let number = 1;
+let number = 2;
+let number = 3;
+console.log(number);
+
+// 7
