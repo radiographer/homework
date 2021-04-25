05c885e3aad2f374e1a8ef85d1d6b5fb36591910
radiographer <lukas.w@gmx.com>
2021-04-25 18:29:51 +0200

    finishedHomework13

--- inne/temp.js
+++ inne/temp.js
@@ -1,58 +1,15 @@
-// function obliczCene(obj) {
-//   return `${produkt.name} kosztuje ${produkt.price}`;
-// }
+let string1 = "primitives";
+let string2 = string1;
+string2 = "primitives after copy and change";
 
-// const produkt = {
-//   name: "Ksiazka JS",
+console.log("string1", string1);
+console.log("string2", string2);
+//zmienia się kopia zmiennej a oryginał pozostaje bez zmian,
 
-//   type: "ksiazka",
+let object1 = { nazwaStringu: "refernces" };
+let object2 = object1;
+object2.nazwaStringu = "references after copy and change ";
 
-//   price: 100,
-// };
-
-// const totalPrice = obliczCene(produkt);
-// console.log(totalPrice);
-
-// function obliczCene(name, type, price) {
-//   return `${name} kosztuje ${price}`;
-// }
-
-// const produkt = {
-//   name: "Ksiazka JS",
-
-//   type: "ksiazka",
-
-//   price: 100,
-// };
-
-// const totalPrice = obliczCene(produkt.name, produkt.type, produkt.price);
-// console.log(totalPrice);
-
-function licznik() {
-  const stan = {
-    ile: 0,
-  };
-  function zwieksz() {
-    stan.ile = stan.ile + 1;
-  }
-  function pokazLicznik() {
-    console.log(stan.ile);
-  }
-  return {
-    ile: stan.ile,
-    zwieksz,
-    pokazLicznik,
-  };
-// }
-const counter = licznik();
-
-console.log(counter);
-
-counter.zwieksz();
-
-// → zwraca nam obiekt ten który jest po słowie return
-// czyli {
-//   ile: 0,
-//   zwieksz: [Function: zwieksz],
-//   pokazLicznik: [Function: pokazLicznik]
-
+console.log("object1", object1.nazwaStringu);
+console.log("object2", object2.nazwaStringu);
+//zmienia się kopia zmiennej ORAZ oryginał, gdyż obie zmienne odnoszą się do tego samego adresu(referencji),
--- lesson13/homework.js
+++ lesson13/homework.js
@@ -131,3 +131,31 @@ pralka.information();
 // 7. Opisz w skrócie na czym polega różnica miedzy kopią, a referencja i stwórz swój własny przykład obrazujący
 //tą różnice +
 // jakie konkretnie typy danych są typami prostymi a jakie złożonymi?
+
+let string1 = "primitives";
+let string2 = string1;
+string2 = "primitives after copy and change";
+
+console.log("string1", string1);
+console.log("string2", string2);
+//zmienia się kopia zmiennej a oryginał pozostaje bez zmian,
+
+let object1 = { nazwaStringu: "refernces" };
+let object2 = object1;
+object2.nazwaStringu = "references after copy and change ";
+
+console.log("object1", object1.nazwaStringu);
+console.log("object2", object2.nazwaStringu);
+//zmienia się kopia zmiennej ORAZ oryginał, gdyż obie zmienne odnoszą się do tego samego adresu(referencji),
+
+// TYPY PROSTE:
+// liczby 1234....
+//stringi 'abcde'
+//boolean true false
+//null
+//undefined
+
+//TYPY ZŁOŻONE:
+//tablice []
+//funkcje ()
+//obiekty {}
