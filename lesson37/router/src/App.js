9d4b2dc45d4f6e71bdab8fe44a889267c4327646
radiographer <lukas.w@gmx.com>
2021-08-05 12:09:08 +0200

    37done

--- lesson37/router/src/App.js
+++ lesson37/router/src/App.js
@@ -51,38 +51,51 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import About from "./pages/About";
 import Home from "./pages/Home";
-
+import Users from "./pages/Users";
 import Topics from "./pages/Topics";
+
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/topics">Topics</Link>
-          </li>
-        </ul>
+    <div className="App">
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+            <li>
+              <Link to="/topics">Topics</Link>
+            </li>
+          </ul>
 
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/topics">
-            <Topics />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+
+            <Route path="/users">
+              <Users />
+            </Route>
+
+            <Route path="/topics">
+              <Topics />
+            </Route>
+
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
   );
 }
 
